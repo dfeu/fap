9fafcbe5c6b4819b383ef87bc89e0c82af31433d
Dr. Daniel Spiekermann <spiekermann.it@gmail.com>
2019-04-16 11:47:40 +0200

    Uhrzeit eingebaut

--- template.php
+++ template.php
@@ -8,6 +8,7 @@ echo "<table border=1><tr><th>Dienst</th><th>aktivieren</th></tr>";
 echo "<tr><td><img src=./pics/whatsapp.jpg width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"wa\">WhatsApp<br>";
 echo "<tr><td><img src=./pics/facebook.png width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"fb\">Facebook<br>";
 echo "<tr><td><img src=./pics/line.png width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"line\">Line<br>";
+echo "<tr><td><img src=./pics/tiktok.png width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"tiktok\">TikTok<br>";
 echo "<tr><td><img src=./pics/knuddels.jpg width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"knuddels\">Knuddels<br>";
 echo "<tr><td><img src=./pics/snapchat.png width=80 height=80></td> <td> <input type=\"checkbox\" name=\"msg[]\" value=\"sc\">Snapchat<br>";
 echo "<tr><td><input type=\"submit\" value=\"Senden\"></td></tr>";
