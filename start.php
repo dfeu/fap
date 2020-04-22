8e2382d39d4006b6b1fddf89b1e39e46803213cb
root <daniel.spiekermann@fernuni-hagen.de>
2020-04-22 16:46:42 +0200

    Update fuer tshark

--- start.php
+++ start.php
@@ -1,8 +1,8 @@
 <?php
 
 echo "Starte tshark zur Überwachung des DNS-Verkehrs";
-#shell_exec("/home/pi/dns2ip.sh &");
-shell_exec("sudo tshark -i wlan0 -f \"src port 53\"  -l -T fields -e dns.a  > /home/pi/dns &");
+shell_exec("/home/pi/dns2ip.sh &");
+#shell_exec("sudo tshark -i wlan0 -f \"src port 53\"  -l -T fields -e dns.a  > /home/pi/dns &");
 echo "<p>";
 echo "<a href=./index.php>Zurück</a>";
 
